bc2a970ef995e42bccf9a9f6525688fc1d0cf817
EntropicEquanimity <64940379+EntropicEquanimity@users.noreply.github.com>
2022-10-03 00:59:59 +0800

    Added FloatingNumbers

--- Assets/Scripts/GUI/FloatingNumber.cs
+++ Assets/Scripts/GUI/FloatingNumber.cs
@@ -1,4 +1,3 @@
-using Sirenix.OdinInspector;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,14 +7,27 @@ namespace SmallTimeRogue
     public class FloatingNumber : MonoBehaviour
     {
         public Sprite[] zeroToNineSprites;
-
-        [Button]
-        public void ShowNumber(int number)
+        private SpriteRenderer[] _sprites = new SpriteRenderer[0];
+        public SpriteRenderer prefabTemplate;
+        public void ShowNumber(int number, Color numberColor)
         {
+            prefabTemplate.color = numberColor;
+            if (_sprites.Length > 0)
+            {
+                for (int i = 0; i < _sprites.Length; i++)
+                {
+                    if (_sprites[i] != null) { Destroy(_sprites[i].gameObject); }
+                }
+            }
             int[] ints = SplitIntIntoList(number);
+            _sprites = new SpriteRenderer[ints.Length];
+            //float totalWidth = 5 * ints.Length * 0.05f;
             for (int i = 0; i < ints.Length; i++)
             {
-                Debug.Log(ints[i]);
+                _sprites[i] = Instantiate(prefabTemplate, transform);
+                _sprites[i].sprite = zeroToNineSprites[ints[i]];
+                _sprites[i].gameObject.SetActive(true);
+                //_sprites[i].transform.localPosition = new Vector3((i == 0 ? 0 : totalWidth / i), 0, 0);
             }
         }
 
